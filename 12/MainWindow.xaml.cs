d3495ddfad5b2be45ff24e48b4eab0d733cc5e00
daktrines <ktkln2003@gmail.com>
2021-12-05 18:33:18 +0300

    Добавьте файлы проекта.

---
+++ 12/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void P_Click(object sender, RoutedEventArgs e)
+        {
+            int rez;
+            int a = Convert.ToInt32(Zn1.Text);
+            Rez.P(a, out rez);
+            P.Text = Convert.ToString(rez);
+        }
+
+        private void S_Click(object sender, RoutedEventArgs e)
+        {
+            int rez;
+            int a = Convert.ToInt32(Zn1.Text);
+            Rez.S(a, out rez);
+            S.Text = Convert.ToString(rez);
+        }
+
+        private void Edinica_Click(object sender, RoutedEventArgs e)
+        {
+            int rez;
+            int a = Convert.ToInt32(Zn2.Text);
+            Rez.Edinica(a, out rez);
+            Edinica.Text = Convert.ToString(rez);
+        }
+
+        private void Desyatki_Click(object sender, RoutedEventArgs e)
+        {
+            int rez;
+            int a = Convert.ToInt32(Zn2.Text);
+            Rez.Desyatki(a, out rez);
+            Desyatki.Text = Convert.ToString(rez);
+        }
+    }
+}
---
+++ 12/Rez.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Rez
+    {
+        public static void P(int a, out int rez)
+        {
+            rez = 4 * a;
+        }
+        public static void S(int a, out int rez)
+        {
+            rez = a * a;
+        }
+        public static void Edinica(int a, out int rez)
+        {
+            rez = a % 10;
+        }
+        public static void Desyatki(int a, out int rez)
+        {
+            rez = a / 10 % 10;
+        }
+
+    }
+}
